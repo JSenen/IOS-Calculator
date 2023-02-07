04913d8ac2f51942092171fe89d61be564ddd5d9
Juan Senen <juansenen@hotmail.com>
2023-02-07 21:35:37 +0100

    Añadido Outlet botones

--- IOS-Calculator/ViewController.swift
+++ IOS-Calculator/ViewController.swift
@@ -8,7 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: -Outlets
+    
+    @IBOutlet weak var resultLabel: UILabel! //Result label
+    
+    //Numbers
+    
+    @IBOutlet weak var number0: UIButton!
+    @IBOutlet weak var number1: UIButton!
+    @IBOutlet weak var number2: UIButton!
+    @IBOutlet weak var number3: UIButton!
+    @IBOutlet weak var number4: UIButton!
+    @IBOutlet weak var number5: UIButton!
+    @IBOutlet weak var number6: UIButton!
+    @IBOutlet weak var number7: UIButton!
+    @IBOutlet weak var number8: UIButton!
+    @IBOutlet weak var number9: UIButton!
+    @IBOutlet weak var numberDecimal: UIButton!
+    
+    //Operator
+    
+    @IBOutlet weak var operatorAC: UIButton!
+    @IBOutlet weak var operatorPlusMinus: UIButton!
+    @IBOutlet weak var operatorPercentage: UIButton!
+    @IBOutlet weak var opertorDivide: UIButton!
+    @IBOutlet weak var operatorMultiply: UIButton!
+    @IBOutlet weak var operatorMinus: UIButton!
+    @IBOutlet weak var operatorPlus: UIButton!
+    @IBOutlet weak var operatorResult: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
